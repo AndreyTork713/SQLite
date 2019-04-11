1cc205253871ed7db1ff56606b071624d5bc4902
Andrey Kropotin <tork713@gmail.com>
2019-04-11 14:28:00 +0700

    11.04.2019_14:27

--- SQLite_1/SQLite_1.py
+++ SQLite_1/SQLite_1.py
@@ -1,1 +1,33 @@
+import sqlite3
+
+
+conn = sqlite3.connect('Chinook_Sqlite.db') # Подключение к базе данных
+
+cursor = conn.cursor()  #Создание объекта курсор
+
+#********** ЧТЕНИЕ ИЗ БАЗЫ **********
+# Делаем SELECT запрос к базе данных, используя обычный SQL-синтаксис
+cursor.execute("SELECT Name FROM Artist ORDER BY Name LIMIT 3")
+
+results = cursor.fetchall()
+results1 = cursor.fetchall()
+
+print(results)
+print(results1)
+
+#***********************************
+#********** ЗАПИСЬ В БАЗУ **********
+# Делаем INSERT запрос к базе данных, используя обычный SQL-синтаксис
+cursor.execute("insert into Artist values (Null,'Aagrh!')")
+# Если мы не просто читаем, но и вносим изменения в базу данных - необходимо сохранить транзакцию
+conn.commit()
+
+#********* Проверка результатов *********
+cursor.execute("SELECT Name FROM Artist ORDER BY Name LIMIT 3")
+
+results = cursor.fetchall()
+print(results)
+
+
+conn.close()
 
